9270ba0e3ee5ae1ecf5df70e910b18970de12dcc
Captain Bear <louis.ritz12@gmail.com>
2025-10-15 11:58:20 +0200

    Added strlen(), strcpy() and strcmp() to standard c libraries.

---
+++ dev/src/bootloader/stage2/restdlibs/string.c
@@ -0,0 +1,39 @@
+#include "string.h"
+
+// Copy SRC to DEST.
+char* strcpy(char* __dest, const char* __src) {
+    char* src = __src;
+    char* dest = __dest;
+    while(src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    return __dest;
+}
+
+// Compare S1 and S2.
+int strcmp(const char* __s1, const char* __s2) {
+    char* s1 = __s1;
+    char* s2 = __s2;
+    while(*s1 == *s2) {
+        if(*s1 == '\0' || *s2 == '\0') {
+            return 0;
+        }
+        s1++;
+        s2++;
+    }
+    return 1;
+}
+
+// Return the length of S.
+size_t strlen(const char* __s) {
+    // Count characters
+    size_t len = 0;
+    char* str = __s;
+    while(*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
+}
---
+++ dev/src/bootloader/stage2/restdlibs/string.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "./../utils.h"
+
+// Copy SRC to DEST.
+char* strcpy(char* __dest, const char* __src);
+
+// Compare S1 and S2.
+int strcmp(const char* __s1, const char* __s2);
+
+// Return the length of S.
+size_t strlen(const char* __s);
